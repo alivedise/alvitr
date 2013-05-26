75806d7dcd8e026ef67cd28243c14de6237a0b16
Alive Kuo <alegnadise@gmail.com>
2013-05-26 22:52:43 +0800

    WIP v2

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,24 @@
  */
 
 var Q = require('q')
-  , MaximumLeaders = 10
+  , MaximumLeaders = 6
   , fs = require('fs')
   , Canvas = require('canvas');
+
+var WIDTH = 851
+  , HEIGHT = 315;
+
+var BackgroundGetter = function(src) {
+  var deferred = Q.defer();
+  fs.readFile(src, function(err, data) {  
+    if (err) {
+      deferred.reject();
+    } else {
+      deferred.resolve(data);
+    }
+  });
+  return deferred.promise;
+};
 
 var IconGetter = function(MID) {
   var deferred = Q.defer();
@@ -43,16 +58,58 @@ function resolve(res, canvas) {
   });
 }
 
-module.exports = function(app){
-  app.post('/form', function(req, res){
-    console.log('body: ' + JSON.stringify(req.body));
+function render(req, res) {
+  var w = 600;
+  var h = 100;
 
-    var canvas = new Canvas(600, 100)
-      , ctx = canvas.getContext('2d');
+  var canvas = new Canvas(w, h)
+    , ctx = canvas.getContext('2d');
+
+  BackgroundGetter(__dirname + '/../public/images/ws.png').then(function(data) {  
+    var img = new Canvas.Image; // Create a new Image
+    img.src = data;
+    ctx.drawImage(img, 0, 200, w, h, 0, 0, img.width, img.height);
 
     if ('background' in req.body) {
-      ctx.fillStyle = req.body.background;
-      ctx.fillRect(0, 0, 600, 100);
+      var lingrad = ctx.createLinearGradient(0, 0, 0, h);
+      lingrad.addColorStop(0, "rgba(0, 0, 0, 1)");
+      lingrad.addColorStop(0.5, "rgba(0, 0, 0, 0.5)");
+      //lingrad.addColorStop(1, req.body.background);
+      lingrad.addColorStop(1, "rgba(0, 0, 0, 1)");
+      ctx.fillStyle = lingrad;
+      //ctx.fillStyle = '#000';
+      ctx.fillRect(0, 0, w, h);
+    }
+
+    if ('name' in req.body) {
+      ctx.fillStyle = '#fff';
+      ctx.font = 'bold 35px Calibri';
+      ctx.textBaseline = 'top';
+      ctx.fillText(req.body.name, 120, 0);
+    }
+
+    if ('id' in req.body) {
+      console.log(req.body.id);
+      ctx.fillStyle = '#0186d1';
+      ctx.font = 'bold 25px Calibri';
+      ctx.textBaseline = 'top';
+      ctx.fillText(req.body.id, w - 120, 0);
+    }
+
+    if ('rank' in req.body) {
+      console.log(req.body.id);
+      ctx.fillStyle = 'gold';
+      ctx.font = 'bold 15px Calibri';
+      ctx.textBaseline = 'top';
+      ctx.fillText('Rank: ' + req.body.rank, w - 100, 50);
+    }
+
+    if ('friend' in req.body) {
+      console.log(req.body.id);
+      ctx.fillStyle = 'silver';
+      ctx.font = 'bold 15px Calibri';
+      ctx.textBaseline = 'top';
+      ctx.fillText('Friends:' + req.body.friend, w - 100, 65);
     }
 
     if ('character' in req.body &&
@@ -74,7 +131,7 @@ module.exports = function(app){
                 var img = new Canvas.Image; // Create a new Image
                 img.src = element;
                 console.log('drawing', img, 'on', index);
-                ctx.drawImage(img, 120 + index * 50, 55, img.width/2, img.height/2);
+                ctx.drawImage(img, 120 + index * 50, 50, img.width/2, img.height/2);
               });
               resolve(res, canvas);
             });
@@ -82,7 +139,7 @@ module.exports = function(app){
             IconGetter(req.body.leaders).then(function(data) {
               var img = new Canvas.Image; // Create a new Image
               img.src = data;
-              ctx.drawImage(img, 120, 60, img.width/2, img.height/2);
+              ctx.drawImage(img, 120, 50, img.width/2, img.height/2);
               resolve(res, canvas);
             });
           }
@@ -92,4 +149,8 @@ module.exports = function(app){
       });
     }
   });
+}
+
+module.exports = function(app){
+  app.post('/form', render);
 };
