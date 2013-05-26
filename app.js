d98878024088408d743832954019cf9738b8f0d5
Alive Kuo <alegnadise@gmail.com>
2013-05-26 18:08:33 +0800

    lol

--- app.js
+++ app.js
@@ -6,7 +6,8 @@
 var express = require('express')
   , routes = require('./routes')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , fs = require('fs');
 
 var app = express();
 
@@ -28,4 +29,39 @@ if ('development' == app.get('env')) {
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
+});
+
+
+/**
+ * Module dependencies.
+ */
+
+var Canvas = require('canvas')
+  , canvas = new Canvas(150, 150)
+  , ctx = canvas.getContext('2d')
+  , fs = require('fs');
+
+ctx.fillRect(0,0,150,150);   // Draw a rectangle with default settings
+ctx.save();                  // Save the default state
+
+ctx.fillStyle = '#09F'       // Make changes to the settings
+ctx.fillRect(15,15,120,120); // Draw a rectangle with new settings
+
+ctx.save();                  // Save the current state
+ctx.fillStyle = '#FFF'       // Make changes to the settings
+ctx.globalAlpha = 0.5;    
+ctx.fillRect(30,30,90,90);   // Draw a rectangle with new settings
+
+ctx.restore();               // Restore previous state
+ctx.fillRect(45,45,60,60);   // Draw a rectangle with restored settings
+
+ctx.restore();               // Restore original state
+ctx.fillRect(60,60,30,30);   // Draw a rectangle with restored settings
+
+
+var out = fs.createWriteStream(__dirname + '/state.png')
+  , stream = canvas.createPNGStream();
+
+stream.on('data', function(chunk){
+  out.write(chunk);
 });
--- public/javascripts/generator.js
+++ public/javascripts/generator.js
@@ -1,8 +1,11 @@
-generator.js
 (function(window) {
   var Generator = {
     init: function() {
-    
+      $('form').change(function() {
+        $('#previewImage').prop('src', '/preview' + new Date().getTime());
+      });
     };
-  }
+  };
+
+  Generator.init();
 }(this));
