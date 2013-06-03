da08e3f16f7585fb2d360772419fd9e675d97e31
Alive Kuo <alegnadise@gmail.com>
2013-06-04 01:10:59 +0800

    v1.0.13r: alpha change

--- javascripts/client.js
+++ javascripts/client.js
@@ -225,7 +225,11 @@
         ctx.fillStyle = param['background-tint'];
         ctx.fillRect(0, 0, IMAGE_CONFIG.WIDTH, IMAGE_CONFIG.HEIGHT);
       } else {
-        ctx.fillStyle = "rgba(255, 255, 255, 0.5)";
+        if (extention == '.png') {
+          ctx.fillStyle = "rgba(255, 255, 255, 0.6)";
+        } else {
+          ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
+        }
         ctx.fillRect(0, 0, IMAGE_CONFIG.WIDTH, IMAGE_CONFIG.HEIGHT);
       }
             // Reset default.
