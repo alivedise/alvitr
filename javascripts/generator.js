bbdf11e4a5d2f6aee50a29303c8889dde39bff2a
Alive Kuo <alegnadise@gmail.com>
2014-01-02 20:39:56 +0900

    6.3.0Release2

--- javascripts/generator.js
+++ javascripts/generator.js
@@ -1,7 +1,7 @@
 (function(window) {
   var JPG_COUNT = 26;
   var PNG_COUNT = 72;
-  var WIKI_PNG_LIMIT = 660;
+  var WIKI_PNG_LIMIT = 1104 - 14;
   var wiki_png_count = 0; // to be calculated.
   var IMAGE_SIZE = {
     'facebook-cover': {
