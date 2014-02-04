047af6a80d106c6d1b0552bedd015bf8a4358c08
Alive Kuo <alegnadise@gmail.com>
2014-02-04 10:28:26 +0100

    6.4.1r2

--- images/background/leaking.js
+++ images/background/leaking.js
@@ -2,11 +2,8 @@
 
 var fs = require('fs');
 var LEAKING = [];
-for (var i = 1; i <= 1104; i++) {
+for (var i = 1; i <= 1200; i++) {
   if (fs.existsSync('./MONS_' + i + '.png')) { // or fs.existsSync
-    
-  } else if (fs.existsSync('./MONS_' + i + '.PNG')) {
-    fs.rename('./MONS_' + i + '.PNG', './MONS_' + i + '.png');
   } else {
     LEAKING.push(i);
   }
--- monster_parser.js
+++ monster_parser.js
@@ -4,7 +4,7 @@ var fs  = require("fs");
 
 fs.readFileSync('./padmonster.csv').toString().split('\r').forEach(function (line) { 
     var a = line.split(',');
-    if ( a.length >= 3 && Number(a[0]) > 0 ) {
+    if ( a.length >= 3 && Number(a[0]) > 0 && a[2] !== '') {
       fs.appendFileSync('./output2.txt', '  "' + Number(a[0]) + '": "No.' + Number(a[0]) + ': ' + a[1] + '-' + a[2] + '",\n');
     }
 });
